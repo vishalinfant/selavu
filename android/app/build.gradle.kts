4f00ebd4bbac6ee69d8c4356ccc42166e69a4ef7
vishalinfant <vishalinfant2@gmail.com>
2025-11-17 20:51:05 +0530

    fix8 - conditional debug code to check CI

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,20 +42,24 @@ android {
         multiDexEnabled = true
     }
 
+    val hasKeyProperties = keystoreProperties["keyAlias"] != null
+
     signingConfigs {
         create("release") {
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
-            storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
-            storePassword = keystoreProperties["storePassword"] as String
+            if (hasKeyProperties) {
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = keystoreProperties["storeFile"]?.let { file(it as String) }
+                storePassword = keystoreProperties["storePassword"] as String
+            }
         }
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig =
+                if (hasKeyProperties) signingConfigs.getByName("release")
+                else signingConfigs.getByName("debug")
         }
     }
 }
